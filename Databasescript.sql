1abcacac98732510da45d220be9ea07662c3e18e
HuyHoang21 <pnhhoang2108@gmail.com>
2021-06-02 10:17:29 +0700

    Add db script

---
+++ Databasescript.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE LoginDB
+Go
+CREATE TABLE [User](
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	FirstName NVARCHAR(128),
+	LastName NVARCHAR(128),
+	UserName VARCHAR(64)
+)
+CREATE TABLE [Account] (
+	ID INT IDENTITY(1,1) PRIMARY KEY,
+	Username VARCHAR(64) UNIQUE,
+	Password BINARY(64),
+)
+CREATE TRIGGER sampleTrigger
+    ON [User]
+    FOR DELETE
+AS
+    DECLARE @maxID int;
+
+    SELECT @maxID = MAX(Id)
+    FROM [User];
+
+    DBCC CHECKIDENT ([User], RESEED, @maxID);
+GO
